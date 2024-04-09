7fd3101838388be6c3126899162a3fbceaa61125
Denis S <denis13th@gmail.com>
2024-04-09 17:06:20 +0300

    Added allowed host

--- pydjango/settings.py
+++ pydjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.138.112']
 
 
 # Application definition
